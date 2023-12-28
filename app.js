ad566d29dabe3a64e1e232557c6dfddc3fc17a88
zia-hussain <syedziashahgillani76@gmail.com>
2023-12-28 15:51:15 +0500

    Registration with socials !

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
-
 const sign_in_btn = document.querySelector("#sign-in-btn");
 const sign_up_btn = document.querySelector("#sign-up-btn");
 const container = document.querySelector(".container");
+const skipBtn = document.getElementById("skip");
+const loginSkip = document.getElementById("l-skip");
 
 sign_up_btn.addEventListener("click", () => {
-container.classList.add("sign-up-mode");
+  container.classList.add("sign-up-mode");
 });
 
 sign_in_btn.addEventListener("click", () => {
-container.classList.remove("sign-up-mode");
+  container.classList.remove("sign-up-mode");
+});
+skipBtn.addEventListener("click", () => {
+  //   alert("hello");
+  container.classList.remove("sign-up-mode");
+});
+loginSkip.addEventListener("click", () => {
+  window.location.replace("./dashboard.html");
 });
