fca83b190b94a9e0ad5026982a513b63fb34000d
zia-hussain <syedziashahgillani76@gmail.com>
2024-01-18 19:25:02 +0500

    Profile page UI set !

--- Profile/profile.js
+++ Profile/profile.js
@@ -4,6 +4,7 @@ import {
   ref,
   push,
   set,
+  update,
   onValue,
   child,
   get,
@@ -73,3 +74,34 @@ const updateUI = (userName) => {
 };
 getUserData();
 
+//    This is specially for profile image
+
+const imgDiv = document.querySelector(".profile-pic-div");
+const img = document.querySelector("#photo");
+const file = document.querySelector("#file");
+const uploadBtn = document.querySelector("#uploadBtn");
+imgDiv.addEventListener("mouseenter", function () {
+  uploadBtn.style.display = "block";
+});
+imgDiv.addEventListener("mouseleave", function () {
+  uploadBtn.style.display = "none";
+});
+file.addEventListener("change", function () {
+  //this refers to file
+  const choosedFile = this.files[0];
+  if (choosedFile) {
+    const reader = new FileReader();
+    reader.addEventListener("load", function () {
+      img.setAttribute("src", reader.result);
+    });
+    reader.readAsDataURL(choosedFile);
+  }
+});
+
+// function updateData() {
+//   let data = update(ref(db, `UsersUid/${id}`),{
+//     nameofuser:{}
+//   });
+//   console.log(data);
+// }
+// updateData();
